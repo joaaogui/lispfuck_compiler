eeda8cd9c7954f6d9f069a7e71ca649174d6193d
Miguel Pimentel <miguelpimentel2013@gmail.com>
2017-11-20 10:17:31 -0200

    Correcting regex for do-before and do-after

--- lispf*ck.py
+++ lispf*ck.py
@@ -21,17 +21,17 @@ tokens_list = [
 
 lexer = ox.make_lexer([
     (tokens_list[0], r'\d+(\.\d*)?(e-?\d+)?'),
-    (tokens_list[2], r'^\s*do'),
-    (tokens_list[3], r'^\s*print'),
-    (tokens_list[3], r'^\s*add'), 
+    (tokens_list[1], r'^\s*do'),
+    (tokens_list[2], r'^\s*print'),
+    (tokens_list[3], r'^\s*add'),
     (tokens_list[4], r'^\s*loop'),
-    (tokens_list[5], r'^\s*('),
-    (tokens_list[6], r'^\s*)'),
-    (tokens_list[7], r'^\s*def')
-    # (tokens_list[8], r'^\s*do-before'),
-    # (tokens_list[9], r'^\s*do-after'),
+    (tokens_list[5], r'[(]'),
+    (tokens_list[6], r'[)]'),
+    (tokens_list[7], r'^\s*def'),
+    (tokens_list[8], r'^\s*do-before'),
+    (tokens_list[9], r'^\s*do-after')
 ])
 
-aux = lexer('def dasbdisbd')
+aux = lexer('do-after')
 print(aux)
 
