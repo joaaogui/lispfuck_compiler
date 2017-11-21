a13fd4d435b8a671b481e9e39afcdd8f6062d6c1
Miguel Pimentel <miguelpimentel2013@gmail.com>
2017-11-21 19:49:40 -0200

    Updating parser_rules and importing click

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 A simple lexer+parser to lispfuck
 
+## Usage
+
+To execute: python3 lispf_ck [lispf_ck_code.lf]
+
 ## Team
 
 | Name | Academic Register (UnB) | GitHub|
--- lispf*ck.py
+++ lispf*ck.py
@@ -1,57 +1,57 @@
 import ox
 import re
+import click
+import pprint
 
-# TOKENS
+# Get ARGS from command line
+@click.command()
+@click.argument('lispf_ck', type=click.File('r'))
 
+# Tokens: List of tokens for lispf*ck
 tokens_list = [
     'NUMBER',
-    'BLOCK',
-    'PRINT',
-    'ADD',
-    'LOOP',
+    'CHARACTER',
     'L_PARANTHESIS',
     'R_PARANTHESIS',
-    'FUNCTION',
-    'FUNCTION_NAME',
-    'DO_BEFORE',
-    'DO_AFTER',
-    'PARAMS',
-    'OPERATORS',
+    'COMMENT',
+    'NEW_LINE',
 ]
 
-
-# LEXER
-
+# Lexer regular expressions to user tokens list
 lexer_rules =  [
     (tokens_list[0], r'\d+(\.\d*)?(e-?\d+)?'),
-    (tokens_list[1], r'^\s*do'),
-    (tokens_list[2], r'^\s*print'),
-    (tokens_list[3], r'^\s*add'),
-    (tokens_list[4], r'^\s*loop'),
-    (tokens_list[5], r'[(]'),
-    (tokens_list[6], r'[)]'),
-    (tokens_list[7], r'^\s*def'),
-    (tokens_list[8], r'^([\'|\"]{1})+(?:(\w+|\W+|\d+|\D+|\s+|\S+|))+([\'|\"]{1})$'),
-    (tokens_list[9], r'^\s*do-before'),
-    (tokens_list[10], r'^\s*do-after'),
-    (tokens_list[11], r'[()]'),
-    (tokens_list[12], r'/\((.*?)\)/'),
+    (tokens_list[1], r'^([\'|\"]{1})+(?:(\w+|\W+|\d+|\D+|\s+|\S+|))+([\'|\"]{1})$'),
+    (tokens_list[2], r'[(]'),
+    (tokens_list[3], r'[)]'),
+    (tokens_list[4], r';[^\n]*'),
+    (tokens_list[5], r'\s+')
 ]
-
-# PARSER
-
-parser_rules = [
-    ('function: def atom', addAction(name, operation)),
-    ('atom: FUNCTION_NAME', lambda x: x),
-    ('def: FUNCTION', lambda x: x),
-]
-
-lexer = ox.make_lexer(lexer_rules)
-parser = ox.make_parser(parser_rules, tokens_list)
-aux = parser(lexer('def f'))
-print(aux)
 
 # Parser functions
+def create_list(l_paranthesis, r_paranthesis):
+    return '()'
 
-def addAction(name, operation):
-    return [name, operation]
+def add_to_list(first_param, second_param):
+    return(first_param,) + second_param
+
+# Parser rules to create AST
+parser_rules = [
+    ('exec_block: L_PARANTHESIS R_PARANTHESIS', lambda x,y: '()'),
+    ('exec_block: L_PARANTHESIS expr R_PARANTHESIS', lambda x,y,z: y),
+    ('expr: atom expr',  lambda x: (x,)),
+    ('expr: atom', lambda x: (x,)),
+    ('atom: exec_block', lambda x: x),
+    ('atom: CHARACTER', lambda x: x),
+    ('atom: NUMBER', lambda x: float(x)),
+]
+
+# Run Application
+def ast(lispf_ck):
+
+	lispfu_ck_code = lispf_ck.read()
+	tokens = lexer(lispfu_ck_code)
+	tree = parser(tokens)
+	pprint.pprint(tree)
+
+if __name__ == '__main__':
+    ast()
