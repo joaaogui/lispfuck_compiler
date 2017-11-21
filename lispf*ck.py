6934d87a8c5d6065677b0df77b8d23691b040b43
João Guilherme <joaaogui@gmail.com>
2017-11-21 21:04:15 -0200

    Fixing space between :

--- lispf*ck.py
+++ lispf*ck.py
@@ -4,11 +4,6 @@ import click
 import pprint
 
 
-# Click Module
-@click.command()
-@click.argument('lispf_ck', type=click.File('r'))
-
-
 # Parser functions
 def create_list(l_paranthesis, r_paranthesis):
     return '()'
@@ -16,6 +11,7 @@ def create_list(l_paranthesis, r_paranthesis):
 
 def add_to_list(first_param, second_param):
     return(first_param,) + second_param
+
 
 # Tokens: List of tokens for lispf*ck
 tokens_list = [
@@ -30,7 +26,8 @@ tokens_list = [
 # Lexer regular expressions to user tokens list
 lexer_rules = [
     (tokens_list[0], r'\d+(\.\d*)?(e-?\d+)?'),
-    (tokens_list[1], r'^([\'|\"]{1})+(?:(\w+|\W+|\d+|\D+|\s+|\S+|))+([\'|\"]{1})$'),
+    (tokens_list[1],
+     r'^([\'|\"]{1})+(?:(\w+|\W+|\d+|\D+|\s+|\S+|))+([\'|\"]{1})$'),
     (tokens_list[2], r'[(]'),
     (tokens_list[3], r'[)]'),
     (tokens_list[4], r';[^\n]*'),
@@ -39,19 +36,23 @@ lexer_rules = [
 
 # Parser rules to create AST
 parser_rules = [
-    ('exec_block : L_PARANTHESIS R_PARANTHESIS', lambda x,y: '()'),
+    ('exec_block : L_PARANTHESIS R_PARANTHESIS', lambda x, y: '()'),
     ('exec_block : L_PARANTHESIS expr R_PARANTHESIS', lambda x, y, z: y),
-    ('expr : atom expr', lambda x,y: (x,) + y),
+    ('expr : atom expr', lambda x, y: (x,) + y),
     ('expr : atom', lambda x: (x,)),
-    ('atom: exec_block', lambda x: x),
-    ('atom: CHARACTER', lambda x: x),
-    ('atom: NUMBER', lambda x: float(x)),
+    ('atom : exec_block', lambda x: x),
+    ('atom : CHARACTER', lambda x: x),
+    ('atom : NUMBER', lambda x: float(x)),
 ]
 
 lexer = ox.make_lexer(lexer_rules)
 parser = ox.make_parser(parser_rules, tokens_list)
 
+# Click Module
 
+
+@click.command()
+@click.argument('lispf_ck', type=click.File('r'))
 # Run Application
 def ast(lispf_ck):
 
@@ -59,6 +60,7 @@ def ast(lispf_ck):
     tokens = lexer(lispfu_ck_code)
     tree = parser(tokens)
     pprint.pprint(tree)
+
 
 if __name__ == '__main__':
     ast()
